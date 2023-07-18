5150f18eec614b53b7c44e97107a45cc3e30484f
chris <chrisrossell1999@gmail.com>
2023-07-18 12:57:57 +0100

    Implemented multipage pdf using datafiles

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+dataframe = pd.read_csv("topics.csv")
 
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1, border=1)
+for index, row in dataframe.iterrows():
+    pdf.add_page()
 
-pdf.set_font(family="Times", style="B", size=8)
-pdf.cell(w=0, h=8, txt="Kenobi!", align="L", ln=1, border=1)
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(x1=10, y1=21, x2=200, y2=21)
 
-pdf.add_page()
 
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1, border=1)
+    pages = row["Pages"]
+    print(pages)
+    topic = row["Topic"]
+    print(topic)
 
-pdf.set_font(family="Times", style="B", size=8)
-pdf.cell(w=0, h=8, txt="Kenobi!", align="L", ln=1, border=1)
+    while pages > 1:
+        pdf.add_page()
+        pages -= 1
 
+# pdf.add_page()
+#
+# pdf.set_font(family="Times", style="B", size=12)
+# pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1, border=1)
+#
+# pdf.set_font(family="Times", style="B", size=8)
+# pdf.cell(w=0, h=8, txt="Kenobi!", align="L", ln=1, border=1)
 
 
 pdf.output("output.pdf")
-
