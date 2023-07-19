66b54cc90ce97e7bafe5425d46ccc39e73a0c4ba
chris <chrisrossell1999@gmail.com>
2023-07-19 12:23:19 +0100

    Added lined pages

--- main.py
+++ main.py
@@ -9,42 +9,33 @@ dataframe = pd.read_csv("topics.csv")
 for index, row in dataframe.iterrows():
     pdf.add_page()
 
+    # header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
 
-    pdf.ln(265)
+    # lines
+    for i in range(20, 280, 10):
+        pdf.line(x1=10, y1=i, x2=200, y2=i)
 
+    # footer
+    pdf.ln(267)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
+    # extra pages
     for i in range(row["Pages"]-1):
         pdf.add_page()
+
+        for i in range(20, 280, 10):
+            pdf.line(x1=10, y1=i, x2=200, y2=i)
 
         pdf.ln(278)
 
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-
-    # my code
-    # pages = row["Pages"]
-    # print(pages)
-    # while pages > 1:
-    #     pdf.add_page()
-    #     pages -= 1
-
-    #
-
-# pdf.add_page()
-#
-# pdf.set_font(family="Times", style="B", size=12)
-# pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1, border=1)
-#
-# pdf.set_font(family="Times", style="B", size=8)
-# pdf.cell(w=0, h=8, txt="Kenobi!", align="L", ln=1, border=1)
 
 
 pdf.output("output.pdf")
